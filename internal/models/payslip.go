bd44e2c72f23c71a2642c944eb4c48c4339743d2
norazlan <realsylo@gmail.com>
2026-02-11 18:04:48 +0800

    refactor PayslipResponse structure to include detailed fields for employee name, gross monthly income, monthly income tax, and net monthly income

--- internal/models/payslip.go
+++ internal/models/payslip.go
@@ -6,13 +6,20 @@ type PayslipRequest struct {
 }
 
 type PayslipResponse struct {
-	Success bool        `json:"success"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
+	EmployeeName       string  `json:"employee_name"`
+	GrossMonthlyIncome float64 `json:"gross_monthly_income"`
+	MonthlyIncomeTax   float64 `json:"monthly_income_tax"`
+	NetMonthlyIncome   float64 `json:"net_monthly_income"`
 }
 
 type ErrorResponse struct {
 	Success bool     `json:"success"`
 	Message string   `json:"message"`
 	Errors  []string `json:"errors,omitempty"`
+}
+
+type SuccessResponse struct {
+	Success bool            `json:"success"`
+	Message string          `json:"message"`
+	Data    PayslipResponse `json:"data"`
 }
